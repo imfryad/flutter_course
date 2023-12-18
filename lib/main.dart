dc556600ca93b7cb9ea59c9f10914d996f216325
Fryad Muhammed <imfryad@gmail.com>
2023-12-18 15:26:12 +0300

    First commit, learn how to use git

---
+++ lib/main.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const Home());
+}
+
+class Home extends StatelessWidget {
+  const Home({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(
+          backgroundColor: Colors.blue[700],
+          title: const Text(
+            'KCT102',
+            style: TextStyle(
+              fontWeight: FontWeight.bold,
+              fontFamily: "english",
+              color: Colors.white,
+            ),
+          ),
+          centerTitle: true,
+        ),
+        body: const Center(
+          child: Text(
+            'سڵاو فریاد چۆنی',
+            style: TextStyle(
+              fontSize: 22.0,
+              fontFamily: "kurdish",
+            ),
+          ),
+        ),
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {},
+          backgroundColor: Colors.blue[200],
+          child: const Icon(Icons.add),
+        ),
+      ),
+    );
+  }
+}
